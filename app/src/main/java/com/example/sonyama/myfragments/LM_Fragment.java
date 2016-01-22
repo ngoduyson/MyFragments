40604f9496871d2400a462531c6f44144bdcb8a3
ngoduyson <ngo.duy.son@framgia.com>
2016-01-22 18:38:07 +0700

    temp

--- app/src/main/java/com/example/sonyama/myfragments/LM_Fragment.java
+++ app/src/main/java/com/example/sonyama/myfragments/LM_Fragment.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 public class LM_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this framgent
-        return inflater.inflate(R.layout.lm_framgent, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.lm_fragment, container, false);
     }
-}
+}gm
